5f05a45c513f3dd44e5d309d5ec2483992a37e49
Jay Lhomme <jhgatohagire@gmail.com>
2024-04-13 13:01:46 -0500

    Tuna

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
-from appliances.kitchen import Dishwasher
+from appliances.kitchen.utility import DishWasher
 from appliances.laundry import Dryer
 from appliances.laundry import Washer
 from appliances.kitchen.utility import Refrigerator
+from appliances.kitchen import CoffeeMaker
+from appliances.appliance import Appliance
+from appliances.kitchen import CanOpener
 
-whirlpool_dishwasher = Dishwasher("black")
+whirlpool_dishwasher = DishWasher("black")
 whirlpool_dishwasher.wash_dishes()
 
-samsung_washer = Washer("red")
+samsung_washer = Washer("red", "normal")
 samsung_dryer = Dryer("red", "gas")
 
 lg_fridge = Refrigerator("stainless")
@@ -14,3 +17,6 @@ lg_fridge.make_ice()
 
 mr_coffee = CoffeeMaker("white")
 mr_coffee.make_coffee()
+
+tin_pin = CanOpener("silver")
+tin_pin.open_can()
